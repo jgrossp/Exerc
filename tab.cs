2673fa7fcf83cf92a504c0daa69b4a726521c522
Jessica Grosskopf <44067719+jgrossp@users.noreply.github.com>
2018-10-27 03:27:53 -0300

    C# - Table - Calculation
    Table - choose a number to perform the calculation
    Tabuada - escolher um número para realizar o cálculo.

---
+++ tab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2)	Faça um programa em C# que calcule a tabuada de multiplicação de um número fornecido pelo usuário.*/
+
+            int tab, num1, num2 = 0;
+
+            Console.WriteLine("Digite um número para realizarmos a tabuada de multiplicação: ");
+            num1 = int.Parse(Console.ReadLine());
+            
+            while (num2<=10)
+            {
+                tab = num1 * num2;
+                Console.WriteLine("A tabuada {0}",num1+"x"+ num2+"=" +tab);
+                num2++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
