a6627dee397f8616bb13785d5f0a56b92103fd2a
Jessica Grosskopf <44067719+jgrossp@users.noreply.github.com>
2018-11-02 01:26:38 -0300

    Using While
    programin C#, that reads the user 10 integer values ​​one at a time and counts how many of these values ​​are negative and how many of these values ​​are positive.

---
+++ aula 5 lista2/aula5 lista2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_5_lista2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  X = 0, NumNega = 0, NumPosi = 0, NumNeu = 0;
+            while (X <= 10)
+            {
+                X = int.Parse(Console.ReadLine());
+                Console.WriteLine("O valor é: {0} ", X );
+                X += 1;
+                
+                if (X %2 == 1) NumNega++;
+                else if (X %2 == 0) NumPosi++;
+                
+                X++;
+            }
+            Console.WriteLine("Quantidade de numeros Negativos: " + NumNega + "\n") ;
+            Console.WriteLine("Quantidade de numeros Positivos: " + NumPosi + "\n");
+            
+            Console.ReadKey();
+        }
+    }
+}
